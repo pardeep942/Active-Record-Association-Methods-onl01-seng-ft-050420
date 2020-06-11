57fdb555ff2593a468240f3bddebcf03178a11fc
PARDEEP KAUR <pkaur84@aol.com>
2020-06-11 05:30:31 +0000

    Done.

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,6 +1,6 @@
 class Genre < ActiveRecord::Base
   has_many :songs
-  has_many :artists, through: :songs
+  has_many :artists, through: :songs                                                                                    
 
   def song_count
     # return the number of songs in a genre
@@ -19,3 +19,4 @@ class Genre < ActiveRecord::Base
     end
   end
 end
+
